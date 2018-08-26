47282e04e7bd52b39e4fd5d4b37774680edfea60
Mohsen <mabbasidesign@yahoo.com>
2018-08-25 22:10:47 -0600

    create post

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from 'axios';
 
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
 class App extends Component {
   state = {
-    posts: []
+    posts: [],
   };
 
   async componentDidMount(){
-    const {data: posts} = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const {data: posts} = await axios.get(apiEndpoint);
     this.setState({ posts });
-  }
+  };
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = {title: "a", body: "b"}
+    const {data: post} = await axios.post(apiEndpoint, obj)
+
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
   handleUpdate = post => {
